edf3b7bc848616caef27af0a3bdf45f84a97431a
tessaamd <146124262+tessaamd@users.noreply.github.com>
2025-06-25 22:47:06 +0300

    добавление типизации

--- src/config/settings.py
+++ src/config/settings.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 class Settings:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    SUPPORT_CHAT_ID = int(os.getenv("SUPPORT_CHAT_ID", -1002887211073))
+    SUPPORT_CHAT_ID = int(os.getenv("SUPPORT_CHAT_ID"))
     MAX_LENGTH = int(os.getenv("MAX_LENGTH", 250))
     DB_NAME = os.getenv("DB_NAME", "support_bot.db")
